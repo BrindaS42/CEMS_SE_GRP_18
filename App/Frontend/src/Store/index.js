4c4af7b23428d112cd795dd576e9a4405ca0214a
Meetbarasara <149007399+Meetbarasara@users.noreply.github.com>
2025-09-26 21:43:25 +0530

    Implemented dashboard

--- App/Frontend/src/Store/index.js
+++ App/Frontend/src/Store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth.slice.js';
+import eventsReducer from './event.slice.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    events: eventsReducer,
   },
 });
 
