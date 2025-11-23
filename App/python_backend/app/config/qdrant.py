a07eb0bbd774c6becfa2af77c989d43ec421ed6f
Brinda-Sorathiya <brindasorathiya626@gmail.com>
2025-11-24 00:53:28 +0530

    setteled up staging environment

--- App/python_backend/app/config/qdrant.py
+++ App/python_backend/app/config/qdrant.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
 
 qdrant_client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
 
-COLLECTION_NAME = "evs"
+COLLECTION_NAME = QDRANT_COLLECTION_NAME
 VECTOR_SIZE = 768  
